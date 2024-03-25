a492ce72c8dfdc568a8510c74833bf23de67c0c7
omoknooni <hihm0609@gmail.com>
2024-03-25 15:27:51 +0900

    feat: add receive_sqs func

--- app.py
+++ app.py
@@ -17,7 +17,16 @@ def receive_message():
     # get SNS message from request
     try:
         msg = json.loads(request.data)
-        msg_process(msg)
+
+        hdr = request.headers.get('x-amz-sns-message-type')
+
+        # confirm for SNS subscription
+        if hdr == 'SubscriptionConfirmation' and 'SubscribeURL' in msg:
+            r = requests.get(msg['SubscribeURL'])
+
+        # get message from SNS
+        if hdr == 'Notification':
+            msg_process(msg['Message'], msg['Timestamp'])
     except Exception as e:
         print(e)
         ret_msg = {
@@ -25,15 +34,6 @@ def receive_message():
         }
         pass
 
-    hdr = request.headers.get('x-amz-sns-message-type')
-
-    # confirm for SNS subscription
-    if hdr == 'SubscriptionConfirmation' and 'SubscribeURL' in msg:
-        r = requests.get(msg['SubscribeURL'])
-
-    # get message from SNS
-    if hdr == 'Notification':
-        msg_process(msg['Message'], msg['Timestamp'])
 
     ret_msg = {
         'status': 'success',
@@ -43,8 +43,26 @@ def receive_message():
     else:
         return make_response(json.dumps(ret_msg), 400)
 
-def msg_process(msg):
-    pass
+def get_sqs_url():
+    res = sqs.get_queue_url(QueueName=os.environ['SQS_QUEUE_NAME'])
+    return res['QueueUrl']
+
+def msg_process(msg, timestamp):
+    # process SNS message
+    parsed_json = json.loads(msg)
+
+    # get message from SQS queue
+    queue_url = get_sqs_url()
+    response = sqs.receive_message(
+        QueueUrl=queue_url,
+        MaxNumberOfMessages=1,
+        WaitTimeSeconds=20,
+    )
+    sqs_msg = response['Messages'][0]
+
+    # take Video URL from SQS message
+    video_url = sqs_msg['Body']
+    generateCaption(video_url)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
